a1694dbf795a61fd174b4a1c10fcd55e45002268
Natalie Acevedo <natalieacevedo25@gmail.com>
2023-01-20 15:12:06 +0000

    Migrated Photo and PhotoPage component to typeScript

---
+++ src/components/Photo.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { Container, Image, Spinner } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Breadcrumbs from "./Breadcrumbs";
+import PhotoThumb from "./PhotoThumb";
+import React from "react";
+import { PhotoData } from "../pages/PhotoPage";
+
+interface PhotoParams {
+  photo: PhotoData;
+  albumId?: string;
+}
+
+function Photo({ photo, albumId }: PhotoParams) {
+  console.log(photo);
+  return (
+    <>
+      <Container>
+        <Breadcrumbs crumbs={photo.breadcrumbs} current={photo.name} />
+        {photo.prev && <PhotoThumb photo={photo.prev} albumId={albumId} />}
+        {photo.next && <PhotoThumb photo={photo.next} albumId={albumId} />}
+      </Container>
+      <Image
+        src={`${process.env.REACT_APP_API_BASE_URL}/api/v1/photo/${photo.id}/file`}
+        alt={photo.filename}
+        fluid
+      />
+    </>
+  );
+}
+
+export default Photo;
---
+++ src/pages/PhotoPage.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import axios from "axios";
+import { Image, Spinner } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import Photo from "../components/Photo";
+import PhotoThumb from "../components/PhotoThumb";
+import { useParams } from "react-router-dom";
+import { APIError } from "../pages/AlbumDetailPage"; //TODO// ORGANIZE ALL TYPESCRIPT VARIABLES SEPARATE FILE
+
+type PhotoId = string | undefined;
+
+type Caption = {
+  language?: string;
+  text?: string;
+};
+
+type BreadCrumbs = {
+  id: string;
+  name: string;
+  date?: string;
+};
+
+export interface PhotoData {
+  id: PhotoId;
+  filename: string;
+  name: string;
+  date_and_time: null;
+  image_url: string;
+  caption: Caption[];
+  breadcrumbs: BreadCrumbs[];
+  prev: {
+    id: string;
+    filename: string;
+    name: string;
+    date_and_time: string;
+  };
+  next: string | null;
+}
+
+function fetchPhoto(photoId: PhotoId) {
+  return axios({
+    url: `${process.env.REACT_APP_API_BASE_URL}/api/v1/photo/${photoId}`,
+  }).then((res) => res.data);
+}
+
+function PhotoPage() {
+  const { photoId } = useParams<{ photoId: string }>();
+
+  const { data, error, isError, isLoading } = useQuery<PhotoData, APIError>(
+    ["photo", photoId],
+    () => fetchPhoto(photoId),
+  );
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <div>{error ? error.message : "Unknown error"}</div>;
+  }
+
+  return data ? <Photo photo={data} /> : "No Photos here";
+}
+export default PhotoPage;
