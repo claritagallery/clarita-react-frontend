c9681b838352671c0f9537e1fc7d6f74dff84541
Natalie Acevedo <natalieacevedo25@gmail.com>
2023-02-15 14:34:34 +0000

    Created navBar and applied basic styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,80 @@
-import React from "react";
-import { Button, Container, Form, FormControl, Nav, Navbar } from "react-bootstrap";
-
+import React, { useState } from "react";
+import logo from "../assets/ccolor.png";
+import logoAzul from "../assets/clogo.png";
+import hamburguer from "../assets/hamburguer.svg";
+import close from "../assets/x.svg";
+//import { Button, Container, Form, FormControl, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-const Header = () => (
-  <Navbar bg="light" expand="md">
-    <Container>
-      <Link to="/">
-        <Navbar.Brand href="/">Clarita</Navbar.Brand>
-      </Link>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-          <Link to="/albums">
-            <Nav.Link>Albums</Nav.Link>
-          </Link>
-          <Link to="/photos">
-            <Nav.Link>Photos</Nav.Link>
-          </Link>
-        </Nav>
-      </Navbar.Collapse>
-      <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="Search (coming soon!)"
-          className="me-2"
-          aria-label="Search (coming soon!)"
-          disabled
-        />
-        <Button disabled variant="outline-success">
-          Search
-        </Button>
-      </Form>
-    </Container>
-  </Navbar>
-);
+
+function Header() {
+  const [open, setOpen] = useState(false);
+  const [closeButton, setCloseButton] = useState(false);
+
+  return (
+    <header className="primary-header">
+      <div className="nav-container">
+        <img src={logoAzul} alt="gallery-logo" width={80} height={80} />
+
+        <button className="hamburguer-botton">
+          <img
+            onClick={() => setOpen((prev) => !prev)}
+            src={hamburguer}
+            className="icon-hamburguer"
+          />
+          <img
+            onClick={() =>
+              setCloseButton((prev) => {
+                console.log(prev);
+                return !prev;
+              })
+            }
+            src={close}
+            className="icon-close"
+          />
+          <span className="visually-hidden">Menu</span>
+        </button>
+        <nav className="primary-navigation">
+          <ul className="nav-list">
+            <li>Albums</li>
+            <li>Photos</li>
+          </ul>
+        </nav>
+      </div>
+      <input type="text" placeholder="search coming soon" />
+    </header>
+  );
+}
+// const Header = () => (
+//   <Navbar bg="light" expand="md">
+//     <Container>
+//       <Link to="/">
+//         <Navbar.Brand href="/">Clarita</Navbar.Brand>
+//       </Link>
+//       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//       <Navbar.Collapse id="responsive-navbar-nav">
+//         <Nav className="me-auto">
+//           <Link to="/albums">
+//             <Nav.Link>Albums</Nav.Link>
+//           </Link>
+//           <Link to="/photos">
+//             <Nav.Link>Photos</Nav.Link>
+//           </Link>
+//         </Nav>
+//       </Navbar.Collapse>
+//       <Form className="d-flex">
+//         <FormControl
+//           type="search"
+//           placeholder="Search (coming soon!)"
+//           className="me-2"
+//           aria-label="Search (coming soon!)"
+//           disabled
+//         />
+//         <Button disabled variant="outline-success">
+//           Search
+//         </Button>
+//       </Form>
+//     </Container>
+//   </Navbar>
+// );
 
 export default Header;
