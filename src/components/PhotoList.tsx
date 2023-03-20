7e7c742c9460096c6ddc19fd3d16006d3c109ab4
Natalie Acevedo <natalieacevedo25@gmail.com>
2023-03-20 10:16:15 +0000

    Photo info div added to picture cell for both small and large screens

--- src/components/PhotoList.tsx
+++ src/components/PhotoList.tsx
@@ -27,22 +27,47 @@ const PhotoList = ({ albumId, data, error, isError, isLoading }: PhotoListParams
 
   if (data) {
     const photos = data.results;
+
     const photosParameters = photos.map((photo) => {
       const { imgSrc, height, width } = getRandomPic();
-      return { src: imgSrc, width: width, height: height, title: photo.name };
+      return {
+        src: imgSrc,
+        width: width,
+        height: height,
+        title: photo.name,
+        date: photo.date_and_time,
+      };
     });
 
     return (
       <div className="gallery-container">
         <h2 className="subtitle">Photos</h2>
         <PhotoAlbum
+          renderPhoto={({ photo, wrapperStyle, renderDefaultPhoto, layout }) => (
+            <div
+              className="picture-cell"
+              style={{
+                position: "relative",
+                ...wrapperStyle,
+                border: "1px solid orange",
+              }}
+            >
+              {renderDefaultPhoto({ wrapped: true })}
+
+              {photo.title && (
+                <div className="picture-info">
+                  <h4> {photo.title}</h4>
+                  <h5>{photo.date}</h5>
+                </div>
+              )}
+            </div>
+          )}
           layout="rows"
           photos={photosParameters}
           defaultContainerWidth={50}
           spacing={2}
           padding={2}
           rowConstraints={(containerWidth) => {
-            console.log("Current containerWidth=", containerWidth);
             if (containerWidth < 300) {
               return {
                 minPhotos: 1,
@@ -64,8 +89,12 @@ const PhotoList = ({ albumId, data, error, isError, isLoading }: PhotoListParams
 
             return {
               minPhotos: 8,
-              maxPhotos: 10,
+              maxPhotos: 9,
             };
+          }}
+          onClick={({ photo, event }) => {
+            // console.log(photo);
+            console.log(event);
           }}
         />
       </div>
