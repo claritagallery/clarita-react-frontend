5466d26518c8543df03ad3ed8d8fefe86f0cf4d7
Natalie Acevedo <natalieacevedo25@gmail.com>
2023-04-30 12:36:43 +0100

    Changed colors of photoList loading dots

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,10 +3,10 @@ function Loader() {
   return (
     <div className="dots-wrapper">
       <div className="dots-container">
-        <div className="yellow dot"></div>
-        <div className="red dot"></div>
-        <div className="blue dot"></div>
-        <div className="violet dot"></div>
+        <div className="one dot"></div>
+        <div className="two dot"></div>
+        <div className="three dot"></div>
+        <div className="four dot"></div>
       </div>
     </div>
   )
