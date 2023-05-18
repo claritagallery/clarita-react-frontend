d255b1ebd428a1c1166d0620d71863560e03d239
Natalie Acevedo <natalieacevedo25@gmail.com>
2023-05-18 12:12:12 +0100

    refactor DetailsDrawer component

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -18,7 +18,7 @@ function Drawer({ photo, isLoading, isBigScreen }: DrawerProps) {
       }`}
     >
       <div className={`title-container ${isOpen ? "drawer-is-open-title" : ""}`}>
-        <h4>{isLoading || !photo ? "Title" : photo.title}</h4>
+        <h4>{isLoading || !photo ? "Photo details..." : photo.title}</h4>
         <button className="toggle-drawer toggle-line">
           <div className="toggle-line"></div>
         </button>
@@ -27,41 +27,57 @@ function Drawer({ photo, isLoading, isBigScreen }: DrawerProps) {
       {(isOpen || isBigScreen) && photo ? (
         <DetailsDrawer photo={photo} />
       ) : (
-        <div className="info-open-drawer">
-          <h4 style={{ color: "#55303e", fontSize: "0.9rem" }}>Photo details ...</h4>
-        </div>
+        <DetailsDrawer />
       )}
     </div>
   )
 }
 
 type DetailsDrawerProps = {
-  photo: PhotoData
+  photo?: PhotoData
 }
 
 function DetailsDrawer({ photo }: DetailsDrawerProps) {
-  return (
-    <div className="info-open-drawer">
-      {photo.description && (
-        <div className="description">
-          <p>{photo.description}</p>
-        </div>
-      )}
+  if (photo) {
+    return (
+      <div className="info-open-drawer">
+        {photo.description && (
+          <div className="description">
+            <p>{photo.description}</p>
+          </div>
+        )}
 
-      <div className="container-brick-items">
-        <div className="date-container brick-item">
-          <h4 className="photo-drawer-date-title">Date</h4>
-          <p>{photo.date_and_time || "date goes here"}</p>
-        </div>
-        <div className="tags brick-item">
-          <h4>Tags</h4>
-          <div className="list-tags">
-            <span>one tag</span>
+        <div className="container-brick-items">
+          <div className="date-container brick-item">
+            <h4 className="photo-drawer-date-title">Date</h4>
+            <p>{photo.date_and_time || "date goes here"}</p>
+          </div>
+          <div className="tags brick-item">
+            <h4>Tags</h4>
+            <div className="list-tags">
+              <span>one tag</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="info-open-drawer">
+        <div className="description"></div>
+
+        <div className="container-brick-items">
+          <div className="date-container brick-item">
+            <h4 className="photo-drawer-date-title">Date</h4>
+          </div>
+          <div className="tags brick-item">
+            <h4>Tags</h4>
+            <div className="list-tags"></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Drawer
